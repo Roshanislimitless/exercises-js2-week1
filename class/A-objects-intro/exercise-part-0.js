b2f9080fdd26164d2e6cd8cf0b4eb12ee0c16693
roshanislimitless <roshan.sapkota2016@gmail.com>
2020-04-22 18:32:43 +0200

    done A

--- class/A-objects-intro/exercise-part-0.js
+++ class/A-objects-intro/exercise-part-0.js
@@ -5,3 +5,10 @@ Describe your own laptop as a JavaScript object
 Try to think of as many properties as you can!
 
 */
+var laptop = {
+    Brand: "lenovo",
+    Ram: "4gb", 
+    screenSize: "14.1 inch"
+ }
+ console.log(laptop);
+ 
